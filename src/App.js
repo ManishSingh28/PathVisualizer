4f44f886d2e501d51fed0c861d8cdc7b3faf3fd6
ManishSingh28 <118452096+ManishSingh28@users.noreply.github.com>
2024-02-10 21:59:57 +0530

    Add files via upload
    Created grid with starting and ending nodes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' ;
+import './App.css';
+import PFV from './PFV/PFV' ;
+
+function App() {
+  return (
+    <div className="App">
+      <PFV></PFV>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PFV/PFV.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+
+import "./PFV.css";
+
+export default class PFV extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
+
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
+      }
+      nodes.push(currentRow);
+    }
+    this.setState({ nodes });
+  }
+
+  render() {
+    const { nodes } = this.state;
+    console.log(nodes);
+    return (
+      <div className="grid">
+        {nodes.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex}>
+              {row.map((node, nodeIndex) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIndex}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                    test={"Foo"}
+                  ></Node>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
